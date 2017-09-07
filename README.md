dfb7649292848722c4a49495c3b462de5fa468a5
Scott Snell <scottysnell@yahoo.com>
2017-09-06 19:12:16 -0700

    update-readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # random_idea_generator
-Simple code to generate a blend of random ideas to help stimulate a person into action.
+Simple code to generate a blend of random ideas to help stimulate a person, in this case, a young child, into action.
 Simple code and functionality to help a user get a random set of "ideas" for activities within a category.
 Here is working example:
 http://fitzgeraldwilliams.com/magic-to-do/
--- random_idea_generator.js
+++ random_idea_generator.js
@@ -1,50 +1,50 @@
 <script>
-    
+
     //ACADEMIC RELATED**************************
-    var arrayMath = ['Do Aleks', 'Practice Math Facts', 'Do Mental Math', 'Do Kumon Multiplication','Do Kumon Division', 'Do Flash Cards', 'Do basic Algebra', 'Do basic Geometry', 'Do Fractions and Decimals'];
-    
+    var arrayMath = ['Do Aleks', 'Practice Math Facts', 'Do Kumon','Do Mental Math', 'Do Kumon Multiplication','Do Kumon Division', 'Do Flash Cards', 'Do basic Algebra', 'Do basic Geometry', 'Do Fractions and Decimals'];
+
     var arrayReading = ['Read a chapter from History outloud', 'Read a Chapter from History', 'Read an educational book', 'read a short story','read a chapter book','read literature'];
 
     var arrayWriting = ['Write a Poem', 'Write a Short Story', 'Write a Chapter Summary from History Book', 'Write a Book Report from any book', 'Write a Movie Plot', 'Write a Magazine Advertisement'];
-    
-    var arraySpelling = ['practice Spelling','copy a paragraph from a book','do an exercise from your spelling book'];    
+
+    var arraySpelling = ['practice Spelling','copy a paragraph from a book','do an exercise from your spelling book'];
     //**************************
-    
-    
+
+
     //MISC EDUCATIONAL**************************
-    var arrayMediaWatchEducational = ['Watch Khan Academy', 'Watch Khan Academy', 'Watch Bruce Yeany Video', 'Watch Documentary', 'Watch TedEd', 'Listen to Jim Weiss','Watch Educational Videos', 'Watch a Historical Movie','learn a new skill on youtube','watch a doc on a National Park','listen to a podcast']; 
-    
+    var arrayMediaWatchEducational = ['Watch Khan Academy', 'Watch Khan Academy', 'Watch Bruce Yeany Video', 'Watch Documentary', 'Watch TedEd', 'Listen to Jim Weiss','Watch Educational Videos', 'Watch a Historical Movie','learn a new skill on youtube','watch a doc on a National Park','listen to a podcast'];
+
     var arraySkillBuilding = ['Practice Typing', 'Practice Cursive Writing', 'Play Chess', 'write a scratch program','do a science experiment','code Dash to do something different','Learn a card game','Make a musical instrument','Sew something','make a simple circuit','practice juggling','make a stop motion picture','make a cartoon sketch'];
-    
+
     var arraySkillLanguage = ['Practice French', 'Learn how to count to 10 in Japanese', 'Learn how to Count to 10 in Chinese', 'Learn how to order food in French', 'Watch a French Language show','learn the basic colors in French','Learn the basic colors in Chinese'];
 
     var arraySkillArt = ['Draw a picture', 'Paint a picture', 'draw something in 3d', 'draw an animal','draw a picture of Paris', 'draw a picture of a historical figure','make a water color painting','create 3d shapes from paper','plant seeds and grow something'];
-    
+
     var arraySkillMemorization = ['Memorize a Poem', 'Memorize a List of Presidents', 'Memorize a List of First Ladies', 'Memorize the 50 states', 'Memorize the 50 State Capitols', 'Memorize the Continents', 'Memorize the countries Europe', 'Memorize the countries in Asia', 'Memorize the the countries in North America', 'Memorize the countries in South America', 'Memorize Multiplication Tables'];
     //**************************
 
     //MUSIC RELATED**************************
     var arrayMusic = ['Learn to play the flute', 'Play the bongo drums', 'Play the Xylophone', 'Practice Singing','learn how to yodel','play piano and sing','play guitar and sing','practice guitar'];
-    //**************************    
-    
+    //**************************
+
     //EXERCISE RELATED**************************
-    var arrayExerciseIndoors = ['Dance to music', 'Pretend you are in a ballet','Play Indoor Catch', 'Play Indoor Baseball', 'Jump Rope', 'try some yoga positions'];  
-    
+    var arrayExerciseIndoors = ['Dance to music', 'Pretend you are in a ballet','Play Indoor Catch', 'Play Indoor Baseball', 'Jump Rope', 'try some yoga positions'];
+
     var arrayExerciseOutdoors = ['Go scootering','Go to the Park', 'Play Soccer', 'Play Tennis', 'Hit Golf Balls', 'Ride your bike', 'dribble the basketball around the block','play frisbee','run as fast as you can (on grass)','jump rope','play hopscotch','practice putting'];
     //**************************
-    
-    //MEDIA RELATED**************************
-    var arrayMediaWatch = ['Watch I Love Lucy', 'Watch Stampy etc.', 'Watch Garfield', 'Watch Word Girl', 'Watch Pink Panther', 'Watch a Movie'];
-    
-    var arrayMediaPlay = ['Play Minecraft', 'Play Beach Buggy Racing', 'Play Toca Hair Salon','play amazon game'];
-    
-    var arrayOtherPlay = ['Play with Dolls', 'Make a fort', 'Bake a cake','Bake cupcakes','make lemonade','do arts and crafts','do an activity book','play jacks','play with legos','play with dash','create an indoor bowling alley','bake bread from scratch','bake cookies','make donuts'];
-    
-    //**************************
-    
-    
 
-    
+    //MEDIA RELATED***************************
+    var arrayMediaWatch = ['Watch I Love Lucy', 'Watch Stampy etc.', 'Watch Garfield', 'Watch Word Girl', 'Watch Pink Panther', 'Watch a Movie'];
+
+    var arrayMediaPlay = ['Play Minecraft', 'Play Beach Buggy Racing', 'Play Toca Hair Salon','create something on tinkercad','play amazon game'];
+
+    var arrayOtherPlay = ['Play with Dolls', 'Make a fort', 'Bake a cake','Bake cupcakes','make lemonade','do arts and crafts','do an activity book','play jacks','play with legos','play with dash','create an indoor bowling alley','bake bread from scratch','bake cookies','make donuts'];
+
+    //**************************
+
+
+
+
     function getUnique(count,curArray) {
       // Make a copy of the array
       var tmp = curArray.slice(curArray);
@@ -56,18 +56,18 @@
         // Since we are only removing one element
         ret.push(removed[0]);
       }
-      return ret;  
+      return ret;
     }
- 
-    
+
+
 </script>
-    
+
     <script>
-    
-    
+
+
     $("#rndEdu").unbind().click(function (e) {
         //populate modal with editable fields
-        
+
             var vMath = getUnique(1,arrayMath);
             var vReading = getUnique(1,arrayReading);
             var vWriting = getUnique(1,arrayWriting);
@@ -78,12 +78,12 @@
             var vSkillArt = getUnique(1,arraySkillArt);
             var vSkillMemorization = getUnique(1,arraySkillMemorization);
             var vSkillMusic = 'practice piano and '+ getUnique(1,arrayMusic);
-        
-        
-        
-            
+
+
+
+
             vHtml = '';
-        
+
             vHtml += '<h4 class="text-center">Math</h4><p class="text-center">'+vMath+'</p>';
             vHtml += '<h4 class="text-center">Reading</h4><p class="text-center">'+vReading+'</p>';
             vHtml += '<h4 class="text-center">Writing</h4><p class="text-center">'+vWriting+'</p>';
@@ -94,62 +94,62 @@
             vHtml += '<h4 class="text-center">Foreign Language</h4><p class="text-center">'+vSkillLanguage+'</p>';
             vHtml += '<h4 class="text-center">Arts</h4><p class="text-center">'+vSkillArt+'</p>';
             vHtml += '<h4 class="text-center">Memorization</h4><p class="text-center">'+vSkillMemorization+'</p>';
-        
-        
+
+
             $( "#results" ).html('');
             $( "#results" ).fadeOut( "fast", function() {
                 $( "#results" ).html('');
               $( "#results" ).fadeIn( "fast", function() {
                 $( "#results" ).html( vHtml );
               });
-            }); 
-        
+            });
+
     });
 
         $("#rndExer").unbind().click(function (e) {
         //populate modal with editable fields
-        
+
             var vExerciseIndoors = getUnique(1,arrayExerciseIndoors);
             var vExerciseOutdoors = getUnique(1,arrayExerciseOutdoors);
-        
-            
+
+
             vHtml = '';
-        
+
             vHtml += '<h4 class="text-center">Exercise</h4><p class="text-center">'+vExerciseIndoors+'<br>'+vExerciseOutdoors+'</p>';
-        
-        
+
+
             $( "#results" ).html('');
             $( "#results" ).fadeOut( "fast", function() {
                 $( "#results" ).html('');
               $( "#results" ).fadeIn( "fast", function() {
                 $( "#results" ).html( vHtml );
               });
-            }); 
-        
+            });
+
         });
 
         $("#rndFun").unbind().click(function (e) {
         //populate modal with editable fields
-        
+
             var vMediaWatch = getUnique(1,arrayMediaWatch);
             var vMediaPlay = getUnique(1,arrayMediaPlay);
             var vOtherPlay = getUnique(1,arrayOtherPlay);
-        
-            
-            
+
+
+
             vHtml = '';
-        
+
             vHtml += '<h4 class="text-center">Play</h4><p class="text-center">'+vOtherPlay+'<br>'+vMediaPlay+'<br>'+vMediaWatch+'</p>';
-        
-        
+
+
             $( "#results" ).html('');
             $( "#results" ).fadeOut( "fast", function() {
                 $( "#results" ).html('');
               $( "#results" ).fadeIn( "fast", function() {
                 $( "#results" ).html( vHtml );
               });
-            }); 
-        
+            });
+
         });
-        
-    </script>    
+
+    </script>
